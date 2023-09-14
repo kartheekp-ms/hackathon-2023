1e2b19d2ee2a62b778d3e65fd5bafd7d55f565ab
Kartheek Penagamuri <kapenaga@microsoft.com>
2023-09-14 13:49:46 -0700

    projectFileDependencyGroups work

--- hackathon-2023/benchmark-json-load/LockFile.cs
+++ hackathon-2023/benchmark-json-load/LockFile.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace benchmark_json_load
@@ -6,5 +8,47 @@ namespace benchmark_json_load
     public class LockFile
     {
         public int Version { get; set; }
+
+        [JsonConverter(typeof(ProjectFileDependencyGroupConverter))]
+        public IList<ProjectFileDependencyGroup> ProjectFileDependencyGroups { get; set; } = new List<ProjectFileDependencyGroup>();
     }
+
+    public class ProjectFileDependencyGroup
+    {
+        public ProjectFileDependencyGroup(string frameworkName, IEnumerable<string> dependencies)
+        {
+            FrameworkName = frameworkName;
+            Dependencies = dependencies;
+        }
+
+        public string FrameworkName { get; }
+
+        public IEnumerable<string> Dependencies { get; }
+    }
+
+
+    public class ProjectFileDependencyGroupConverter : JsonConverter<IList<ProjectFileDependencyGroup>>
+    {
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer, IList<ProjectFileDependencyGroup> value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<ProjectFileDependencyGroup> ReadJson(JsonReader reader, Type objectType, IList<ProjectFileDependencyGroup> existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            var jObject = JObject.Load(reader);
+            var groups = new List<ProjectFileDependencyGroup>();
+
+            foreach (var prop in jObject.Properties())
+            {
+                var group = new ProjectFileDependencyGroup(prop.Name, prop.Value.ToObject<IList<string>>());
+                groups.Add(group);
+            }
+
+            return groups;
+        }
+    }
+
 }
--- hackathon-2023/benchmark-json-load/Program.cs
+++ hackathon-2023/benchmark-json-load/Program.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 using System.IO;
 
 namespace benchmark_json_load
@@ -11,7 +12,9 @@ namespace benchmark_json_load
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            //_ = BenchmarkRunner.Run(typeof(Program).Assembly);
+            //new Benchmarks().JObjectLoad();
+            new Benchmarks().Deserialize();
         }
     }
 
@@ -41,11 +44,26 @@ namespace benchmark_json_load
                 }
             }
 
+            //version
             var jObject = JObject.Load(jsonReader, DefaultLoadSettings);
             var versionToken = jObject["version"];
             int version = versionToken.Value<int>();
 
-            return new LockFile() { Version = version };
+            //projectFileDependencyGroups
+            JObject projectFileDependencyGroupsJson = jObject["projectFileDependencyGroups"] as JObject;
+            var projectFileDependencyGroups = new List<ProjectFileDependencyGroup>(projectFileDependencyGroupsJson.Count);
+            foreach (var child in projectFileDependencyGroupsJson)
+            {
+                var jDependenciesArray = child.Value as JArray;
+                var dependencies = new List<string>(jDependenciesArray.Count);
+                foreach (var dependency in jDependenciesArray)
+                {
+                    dependencies.Add(dependency.Value<string>());
+                }
+                projectFileDependencyGroups.Add(new ProjectFileDependencyGroup(child.Key, dependencies));
+            }
+
+            return new LockFile() { Version = version, ProjectFileDependencyGroups = projectFileDependencyGroups };
         }
 
         [Benchmark]
@@ -55,7 +73,7 @@ namespace benchmark_json_load
             using var textReader = new StreamReader(assetsFileStream);
             using var jsonReader = new JsonTextReader(textReader);
 
-            JsonSerializer serializer = new();
+            JsonSerializer serializer = JsonSerializer.Create();
             var lockFile = serializer.Deserialize<LockFile>(jsonReader);
 
             return lockFile;
